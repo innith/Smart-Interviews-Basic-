90f22f386af8be21319439b349c781fb5c653191
Nithin Reddy Bathula <nithinreddybathula.i@gmail.com>
2022-11-22 21:36:34 +0530

    2nd Answer

---
+++ 2.Print unique elements of array/Print_unique_elements_of_array.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        for(int p =0;p<n;p++){
+            arr[p]=sc.nextInt();
+        }
+        for(int i =0;i<n;i++){
+            int count =0;
+            for(int j =0;j<n;j++){
+                if(arr[i]==arr[j]){
+                    count ++; 
+                    if(count>1) continue;
+                }if(count>1) continue;
+            }if(count==1) 
+            System.out.printf(arr[i]+" "); 
+        }
+        
+    }
+}
